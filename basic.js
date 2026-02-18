d30f2a40b8131eca9dcb8e5cf8765b849d4c77cd
Samra <samramuqadasvu1@gmail.com>
2026-02-18 21:05:42 +0500

    Day 3 interview prep added

--- basic.js
+++ basic.js
@@ -87,3 +87,36 @@ if (operator === "+") {
 } else {
   console.log("Invalid operator");
 }
+// arr large number 
+function largest(arr){
+  let max = arr[0];
+
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i] > max){
+      max = arr[i];
+    }
+  }
+
+  return max;
+}
+// small number function smallest(arr){
+  let min = arr[0];
+
+  for(let i = 1; i < arr.length; i++){
+    if(arr[i] < min){
+      min = arr[i];
+    }
+  }
+
+  return min;
+//function countEven(arr){
+  let count = 0;
+
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 === 0){
+      count++;
+    }
+  }
+
+  return count;
+
